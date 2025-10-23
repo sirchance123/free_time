2e1da0372b19b5c51f2968fdfacb24f2ebb4d962
sirchance123 <chancehuxter@gmail.com>
2025-10-23 18:24:22 +0000

    almost done with east mode

---
+++ lottery_machine.py
@@ -0,0 +1,9 @@
+import random
+accept = input("Do you want to play the lottery? Answe Yes or No : ")
+accept_real = accept.capitalize()
+print(accept_real)
+if accept_real == "Yes":
+    mode = input("Do you want...Easy...Normal...or Hard mode? :")
+    mode_real = mode.capitalize()
+    if mode_real == "Easy" : 
+
